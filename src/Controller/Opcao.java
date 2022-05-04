b643bf71b4d8d22e8a6ab16b3a8ba95ca0b67d99
vicaSouzza <vitoriasouzza.vrs@gmail.com>
2022-05-03 21:39:48 -0300

    Ajustando view e menu opcao

--- src/Controller/Opcao.java
+++ src/Controller/Opcao.java
@@ -1,13 +1,13 @@
 package Controller;
 
 public enum Opcao {
-	LISTA_ALUNOS(1, "Listar todos os funcionarios cadastrados"), 
-	LISTA_CURSOS(2, "Listar todos os departamentos cadastrados"), 
-	LISTA_ALUNOS_FROM_CURSO(3, "Listar todos os funcionarios de um departamento escolhido"), 
-	LISTA_CURSOS_FROM_ALUNO(4, "Listar todos os departamentos que um funcionario escolhido trabalha"),
-	ADICIONA_ALUNO(5, "Cadastrar um funcionario"),
-	ADICIONA_CURSO(6, "Cadastrar um novo departamento"),
-	ADICIONA_RELACAO(7, "Adicionar uma relacao entre um funcionario e um departamento"),
+	LISTA_ALUNOS(1, "Listar todos os alunos cadastrados"),
+	LISTA_CURSOS(2, "Listar todos os cursos cadastrados"),
+	LISTA_ALUNOS_FROM_CURSO(3, "Listar todos os alunos de um curso escolhido"),
+	LISTA_CURSOS_FROM_ALUNO(4, "Listar todos os cursos que um aluno escolhido é cadastrado"),
+	ADICIONA_ALUNO(5, "Cadastrar um aluno"),
+	ADICIONA_CURSO(6, "Cadastrar um novo curso"),
+	ADICIONA_RELACAO(7, "Adicionar uma relacao entre um aluno e um curso"),
 	SAIR(0, "Sair do programa e salvar os dados");
 	
 	public String descricao;
--- src/View/ViewTerminal.java
+++ src/View/ViewTerminal.java
@@ -1,100 +1,152 @@
 package View;
 
 import java.util.Collection;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
+import Controller.Controller;
 import Controller.Opcao;
 import Entidades.Aluno;
 import Entidades.Curso;
 import Repository.Repository;
 
-public class ViewTerminal implements View{
-	
-	private Repository repositorio;
-	
-	public ViewTerminal(Repository repositorioCarregado) {
-		this.repositorio = repositorioCarregado;
-	}
+public class ViewTerminal implements View {
 
-	@Override
-	public Aluno adicionaAluno() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    private Repository repositorio;
 
-	@Override
-	public Curso adicionaCurso() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    public ViewTerminal(Repository repositorioCarregado) {
 
-	@Override
-	public Curso getCursoFromLista(Repository repository) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+        this.repositorio = repositorioCarregado;
+    }
 
-	@Override
-	public Aluno getAlunoFromLista(Repository repository) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public Opcao menu() {
+        try {
 
-	@Override
-	public void listarAlunos(Repository repository) {
-		  /**
-	     * Retorna todos alunos
-	     * alunoCSV precisa ser carregado a partir do database (repository)
-	     * */
-		
-		Collection<Aluno> alunos = repositorio.getAllAlunos();
-		
-		System.out.println("Todos os alunos cadastrados: ");
-		
-        for(Aluno aluno : alunos) {
+            System.out.println("Escolha uma das opções");
+            for (Opcao o : Opcao.values()) {
+                System.out.println("" + o.codigo + " - " + o.descricao);
+
+            }
+
+            @SuppressWarnings("resource")
+            Scanner in = new Scanner(System.in);
+            try {
+                String entrada = in.nextLine();
+                int opcaoCodigo = Integer.parseInt(entrada);
+
+                if (!Controller.opcoesByCodigos.keySet().contains(opcaoCodigo)) {
+                    throw new InputMismatchException("A opção escolhida não corresponde a nunhum código listado");
+                }
+                return Controller.opcoesByCodigos.get(opcaoCodigo);
+
+            } catch (NumberFormatException e) {
+                throw new InputMismatchException("A opção de entranda não é um número inteiro");
+            }
+        } catch (InputMismatchException e) {
+            System.out.println(e.getMessage());
+            menu();
+        }
+
+        return null;
+    }
+
+
+    @Override
+    public Aluno adicionaAluno() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Curso adicionaCurso() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Curso getCursoFromLista(Repository repository) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Aluno getAlunoFromLista(Repository repository) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void listarAlunos(Repository repository) {
+        /**
+         * Retorna todos alunos
+         * alunoCSV precisa ser carregado a partir do database (repository)
+         * */
+
+        Collection<Aluno> alunos = repositorio.getAllAlunos();
+
+        System.out.println("Todos os alunos cadastrados: ");
+
+        for (Aluno aluno : alunos) {
             System.out.println(aluno);
         }
-	}
+    }
 
-	@Override
-	public void listarCursos(Repository repository) {
-		System.out.println("Todos os Cursos cadastrados: ");
-		for(Curso curso : repositorio.getAllCursos()){
-			System.out.println(curso);
-		}
-	}
+    @Override
+    public void listarCursos(Repository repository) {
+        System.out.println("Todos os Cursos cadastrados: ");
+        for (Curso curso : repositorio.getAllCursos()) {
+            System.out.println(curso);
+        }
+    }
 
-	@Override
-	public void listarCursosFromAluno(Repository repository, Aluno aluno) {
-		System.out.println("Todos os cursos do Aluno: "+ aluno.getNome());
-		
-		for(Curso curso : repository.getCursoFromAluno(aluno)) {
-			System.out.println(curso);
-		}
-		
-	}
+    @Override
+    public void listarCursosFromAluno(Repository repository, Aluno aluno) {
+        System.out.println("Todos os cursos do Aluno: " + aluno.getNome());
 
-	@Override
-	public void listarAlunosFromCursos(Repository repository, Curso curso) {
-		 /**
-	     * Retorna uma lista de alunos que possuem o curso fornecido
-	     * */
-		System.out.println("Todos os alunos do curso: "+curso.getNome());
-		
-		for(Aluno aluno :  repository.getAlunosFromCurso(curso)) {
-			System.out.println(aluno);
-		}
-	}
+        for (Curso curso : repository.getCursoFromAluno(aluno)) {
+            System.out.println(curso);
+        }
 
-	@Override
-	public void adicionaRelacao(Aluno aluno, Curso curso) {
-		// TODO Auto-generated method stub
-		
-	}
+    }
 
-	@Override
-	public Opcao menu() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public void listarAlunosFromCursos(Repository repository, Curso curso) {
+        /**
+         * Retorna uma lista de alunos que possuem o curso fornecido
+         * */
+        System.out.println("Todos os alunos do curso: " + curso.getNome());
 
+        for (Aluno aluno : repository.getAlunosFromCurso(curso)) {
+            System.out.println(aluno);
+        }
+    }
+
+    @Override
+    public void adicionaRelacao(Aluno aluno, Curso curso) {
+        // TODO Auto-generated method stub
+
+    }
+
+    public Aluno getNewAluno(){
+        System.out.println("Entre com os dados do aluno: ");
+        String id = getIdAluno();
+        String nome = getNomeAluno();
+        return new Aluno(id, nome);
+    }
+
+    private String getNomeAluno() {
+        return getString();
+    }
+
+    private String getIdAluno() {
+        return getString();
+    }
+    public String getString() {
+        Scanner in = new Scanner(System.in);
+        String str = in.nextLine();
+        return str.trim();
+    }
 }
+
+
