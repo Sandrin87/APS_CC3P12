bdfb6aef3b34e10996f40a12e0922c0ee0ff3388
Mantovaniz <67487622+Mantovaniz@users.noreply.github.com>
2022-05-12 22:06:16 -0300

    Repository and Cadastro 12.05.2022

--- src/Entidades/Cadastro.java
+++ src/Entidades/Cadastro.java
@@ -1,5 +1,57 @@
 package Entidades;
 
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
+import java.util.Objects;
+
+
 public class Cadastro {
     
+    Map<Aluno, Set<Curso>> aluno = new TreeMap<>();//vetor aluno//
+	Map<Curso, Set<Aluno>> curso = new TreeMap<>();
+
+    Map<String, Aluno> listaAlunoPorId = new TreeMap<>();
+
+
+    public Aluno getAlunoFromId (String id){
+        return listaAlunoPorId.get(id);
+
+    }
+
+    public Set<Aluno> getAluno(){ 
+        return aluno.keySet();
+
+    }
+
+    public void addAlunos (Aluno aluno){
+        this.aluno.put(aluno, new TreeSet<>());
+        this.listaAlunoPorId.put(aluno.getId(aluno));
+    }
+
+    public Set<Curso> getCurso(){
+        return curso.keySet();
+    }
+
+    public void addCurso (Curso curso){
+        this.curso.put(curso, new TreeSet<>());
+    }
+
+    public Set<Curso> getCursoFromAluno(String id){
+        return aluno.get(new Aluno(id, ""));
+    }
+
+//Curso é objeto, o curso ele é a variável que vai armazenar o objeto// //o objeto tem que ter uma class com o msm nome do objeto//
+
+    public Set<Aluno> getAlunoFromCurso (Curso curso){ 
+        return curso.getCurso(curso);
+    }
+
+    public void addRelacaoAlunoCurso(Aluno aluno, Curso curso){
+        this.curso.get(curso).add(aluno);
+        this.aluno.get(aluno).add(curso);
+    }
+
+
 }
